8d974cd7aa57fcd7b7b818193a42e07d1be05b0b
andrewszw <zacharyandrews93@gmail.com>
2014-09-11 06:01:23 -0400

    Documented arrayList.c

--- lab3/arrayList.c
+++ lab3/arrayList.c
@@ -2,24 +2,55 @@
 #include <stdio.h>
 #include "arrayList.h"
 
+/**
+ * @author: Zachary Andrews
+ *
+ * @userid: andrewszw
+ *
+ * This program is designed to implement an "arraylist" in C.
+ * This is achieved by passing a parameter to the initialize function
+ * that indicates the type of element to be stored in the array.
+ * There is a function to initialize the array, add an element to the array,
+ * remove an element from the array, and print the array
+ **/
+
 //getSize not accessed outside of this file so declare it to be static
 //static essentially makes it private to this file 
 static int getSize(primitiveType type);
 
+/**
+ * initialize: This function initializes our array by passing it
+ *             a primitive type an allocating the proper amount of space
+ *             for an array of that type.
+ *
+ * parameters: primitiveType type (type of element to be stored in array)
+ *
+ * returns: returns a pointer to the newly initialized array
+ **/
 arrayList * initialize(primitiveType type)
 {
     arrayList * al = (arrayList*)malloc(sizeof(arrayList));
     al->arraySize = 4;
     al->numElements = 0;
     al->type = type;
-    al->elementSize = getSize(type);
-    //al = malloc((al->elementSize)*4); 
+    al->elementSize = getSize(type); 
     al -> array = (void*)malloc((getSize(type)) * al ->arraySize);
     return al;
 }
 
+/**
+ * getSize: This function gives us the number of bytes in a variable
+ *          of the primitive type that is passed.
+ *
+ * parameters: primitiveType type (type of element to calculate bytes of)
+ *
+ * returns: an integer representing the number of bytes of the primitive type
+ **/
 int getSize(primitiveType type)
 {
+
+    // compare types and use sizeof(type) operator to get value
+
     if(type == charType)
         return sizeof(char);
     else if(type == shortType)
@@ -30,6 +61,15 @@ int getSize(primitiveType type)
         return 0;
 }
 
+/**
+ * addElement: This function adds an element to the end of the array. It will double
+ *             the size of the array and copy all of the old elements to the new array.
+ *
+ * parameters: arrayList * arylstP (represents our array)
+ *             void * element (represents element to be added)
+ *
+ * returns: void
+ **/
 void addElement(arrayList * arylstP, void * element)
 {
    int i;
@@ -37,14 +77,25 @@ void addElement(arrayList * arylstP, void * element)
    {
        char * newarray = (char *) malloc(2 * arylstP -> numElements * arylstP -> elementSize);
        
+       // copy elements to new array
+       
        for(i = 0; i < (arylstP -> numElements * arylstP -> elementSize); i++)
        {
            newarray[i] = ((char *)arylstP->array)[i]; 
        }
+
+       // free memory of old array
+
        free(arylstP -> array);
        arylstP->array = newarray;
+
+       // properly reset the array size
+
        arylstP->arraySize = arylstP -> arraySize*2;
    }
+
+   // add element to array based proper type
+
    if(arylstP -> type == charType)
    {
        ((char *)arylstP -> array)[arylstP -> numElements] = *((char *)element);
@@ -57,14 +108,32 @@ void addElement(arrayList * arylstP, void * element)
    {
        ((int *)arylstP -> array)[arylstP -> numElements] = *((int *)element);
    }
+
+   // increment the number of elements in the array
+
    arylstP -> numElements++;
 }
 
+/**
+ * removeElement: This function will remove an element from the array at
+ *                the specified index that is passed as a parameter.
+ *
+ * parameters: arrayList * arylstP (represents our array)
+ *             int index (index of the array to remove element)
+ *
+ * returns: void
+ **/
 void removeElement(arrayList * arylstP, int index)
 {
    int i;
+   
+   // shift all values from index down one
+   
    for(i = index; i < arylstP ->numElements - 1; i++)
    {
+
+       // handle each type case
+
        if(arylstP -> type == charType)
        {
            char* value = (char*) arylstP -> array;
@@ -81,12 +150,26 @@ void removeElement(arrayList * arylstP, int index)
            value[i] = value[i + 1];
        }
    }
+
+   // decrement the number of elements
+
    arylstP -> numElements--;
 }
 
+/**
+ * printArray: This function simply prints each element that is stored
+ *             in given array.
+ *
+ * parameters: arrayList * arylstP (represents the array)
+ *
+ * returns: void
+ **/
 void printArray(arrayList * arylstP)
 {
    int i;
+   
+   // create temp variables for each primitive type to be anded with given values
+
    char* temp1;
    short* temp2;
    int* temp3;
